749993f32fcfb9edf8824c2dd81323cc2f842ac9
kmillanmx <102334878+kmillanmx@users.noreply.github.com>
2024-11-15 14:24:06 -0600

    Create EjemploIgualdad.java

---
+++ OperadoresRelacionales/EjemploIgualdad.java
@@ -0,0 +1,12 @@
+public class EjemploIgualdad {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        if (a == b) {
+            System.out.println("a es igual a b");
+        } else {
+            System.out.println("a no es igual a b");
+        }
+    }
+}
